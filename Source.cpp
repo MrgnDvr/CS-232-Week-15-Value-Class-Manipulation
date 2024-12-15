deef970de0e7fcbe9f2185eaa494790476d351b7
MrgnDvr <160530125+MrgnDvr@users.noreply.github.com>
2024-12-14 17:47:04 -0800

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+//#include"One.h"
+#include"Two.h"
+using namespace std;
+int main() {
+Two myTwoObject;
+myTwoObject.showMyParentsValue();
+/*One myOneObject;
+cout << myOneObject.getMyClassVal() << endl;*/
+return 0;
+}
